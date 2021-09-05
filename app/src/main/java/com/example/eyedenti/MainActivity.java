67e3950d9ccac55f593e419f1ec09329880cc015
lhj99 <inklhj99@gmail.com>
2021-09-05 22:33:07 +0900

    open, close, suction, anesthesia -> mp4 추가

--- app/src/main/java/com/example/eyedenti/MainActivity.java
+++ app/src/main/java/com/example/eyedenti/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.eyedenti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
