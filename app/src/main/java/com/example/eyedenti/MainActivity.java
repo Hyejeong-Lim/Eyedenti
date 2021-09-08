87ab8394c048fbc5666232970f559fca1897ebcb
DESKTOP-FAI9CGK\jo <1_dll@naver.com>
2021-09-08 15:00:49 +0900

    상태바 제거

--- app/src/main/java/com/example/eyedenti/MainActivity.java
+++ app/src/main/java/com/example/eyedenti/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.eyedenti;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.VideoView;
@@ -15,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
     }
 }
